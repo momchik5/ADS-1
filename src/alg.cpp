c9af58b9f5a9cb48d5867c632dd08cd01b4d65d9
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-02-28 01:12:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,39 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    for (uint64_t i = 2; i < value; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
-
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t count = 0;
+    uint64_t value=1 ;
+    while (count<n) {
+        value++;
+        if (checkPrime(value)) {
+            count++;
+        }
+    }
+    return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    if (value <= 1) return 0;
+    value++;
+    while (!checkPrime(value)) {
+        value++;
+    }
+    return value;
 }
-
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+   uint64_t sum = 0;
+    for (uint64_t y = 2; y < hbound; y++) {
+        if (checkPrime(y)) {
+            sum += y;
+        }
+    }
+    return sum;
 }
